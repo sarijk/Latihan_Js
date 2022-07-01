b94b0d81d1fa91d2c3d781fa595e616ad28e526c
sarijk <jatuksari@gmail.com>
2022-07-02 05:46:05 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const foods = ['Bakso', 'Rawon', 'Soto', 'Seblak'];
+//saya mau menampilkan isi dari array foods yang memiliki index2
+//console.log(foods[2]);
 
-// console.log(...food);
+// console.log(...foods);
 
 // const food1 = foods[0];
 // const food2 = foods[1];
@@ -11,4 +13,31 @@ const foods = ['Bakso', 'Rawon', 'Soto', 'Seblak'];
 //menghapus dari index 0 sebanyak 1 elemen
 // foods.splice(0,1);
 // console.log(foods);
+
+//Destructuring Array
+
+
+const profile ={
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 18,
+}
+
+const capital = new Map([
+    ['Jakarta', 'Indonesia'],
+    ['London', 'England'],
+    ['Tokyo', 'Japan'],
+]);
+
+/*output: 3*/
+// console.log(capital.size);
+
+/*output: England
+    Kalau value-nya yang dipanggil output undefined */
+// console.log(capital.get('London'));
+
+/* output: Turkey
+    Menambahkan key-value baru lalu menampilkan value barunya */
+// capital.set('Ankara', 'Turkey');
+// console.log(capital.get('Ankara'));
 
