6c1a14c201288d881f5a10ef7a8cb648b273f989
sarijk <jatuksari@gmail.com>
2022-07-01 15:25:14 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const foods = ['Bakso', 'Rawon', 'Soto', 'Seblak'];
+
+// console.log(...food);
+
+// const food1 = foods[0];
+// const food2 = foods[1];
+// const food3 = foods[2];
+// const food4= foods[3];
+// console.log(food1, food2, food3, food4);
+
+//menghapus dari index 0 sebanyak 1 elemen
+// foods.splice(0,1);
+// console.log(foods);
+
